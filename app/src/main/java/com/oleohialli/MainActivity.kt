bc4c9365331db396a07b1ad2013b6431e408646f
oleoh <alli.oleohi@thegiggroupng.com>
2021-07-30 17:10:23 +0100

    Navigation components and other dependencies set up

--- app/src/main/java/com/oleohialli/MainActivity.kt
+++ app/src/main/java/com/oleohialli/MainActivity.kt
@@ -2,10 +2,29 @@ package com.oleohialli
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.oleohialli.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment_main) as NavHostFragment
+        navController = navHostFragment.findNavController()
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
---
+++ app/src/main/java/com/oleohialli/views/LessonFragment.kt
@@ -0,0 +1,7 @@
+package com.oleohialli.views
+
+import androidx.fragment.app.Fragment
+import com.oleohialli.R
+
+class LessonFragment : Fragment(R.layout.fragment_lesson) {
+}
